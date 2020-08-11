c93828551ddceeec0e364375aa129db75b0339b8
william <wmallett@hy-vee.com>
2020-08-11 18:09:31 -0500

    adding more strategies

--- router.js
+++ router.js
@@ -1,5 +1,14 @@
 const {signup} = require('./controllers/authentication');
+const passportService = require('./services/passport');
+const passport = require('passport');
+
+// session false because we want stateless jwt
+const requireAuth = passport.authenticate('jwt', {session: false});
 
 module.exports = (app) => {
+    app.get('/', requireAuth, (req, res) => {
+        res.send({hi: 'there'});
+    });
+
     app.post('/signup', signup);
 };
--- services/passport.js
+++ services/passport.js
@@ -3,3 +3,38 @@ const User = require('../models/user');
 const config = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+const LocalStrategy = require('passport-local');
+
+// Create local strategy
+const localOptions = {usernameField: 'email'};
+const localLogin = new LocalStrategy(localOptions, (email, password, done) => {
+    // verify username and password
+    // call done with user if has correct creds
+    // else call done with false
+});
+
+
+// Setup options for jwt strategy
+const jwtOptions = {
+    jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+    secretOrKey: config.secret
+};
+
+// create jwt strategy
+const jwtLogin = new JwtStrategy(jwtOptions, (payload, done) => {
+    // See if the user ID in the payload exists in our db
+    // if so, call done with that user 
+    // else call done without a user object
+    User.findById(payload.sub, (err, user) => {
+        if (err) return done(err, false);
+
+        if (user) {
+            done(null, user);
+        } else {
+            done(null, false);
+        }
+    });
+});
+
+// Tell passport to use this strategy
+passport.use(jwtLogin);
